0c74c93a6baf62f3c98047028bb3589fe9b74550
Thomas Auinger <thomas@auinger.de>
2021-11-29 11:58:28 +0100

    implemented response

--- src/main/java/com/example/pizza/order/OrderSoapEndpoint.java
+++ src/main/java/com/example/pizza/order/OrderSoapEndpoint.java
@@ -1,12 +1,15 @@
 package com.example.pizza.order;
 
 import com.example.pizza.order.soap.PlaceOrderRequest;
+import com.example.pizza.order.soap.PlaceOrderResponse;
 import com.example.pizza.order.soap.QuantityMap;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,15 +24,31 @@ public class OrderSoapEndpoint {
         this.orderService = orderService;
     }
 
+
     @PayloadRoot(namespace = NAMESPACE, localPart = "placeOrderRequest")
     @ResponsePayload
-    public void placeOrder(@RequestPayload PlaceOrderRequest placeOrderRequest) {
-        System.out.println(placeOrderRequest);
-        orderService.placeOrder(
+    public PlaceOrderResponse placeOrder(@RequestPayload PlaceOrderRequest placeOrderRequest) {
+        Order order = orderService.placeOrder(
                 placeOrderRequest.getPhoneNumber(),
                 toHashMap(placeOrderRequest.getProductQuantities())
         );
+        return toPlaceOrderResponse(order);
     }
+
+
+    private PlaceOrderResponse toPlaceOrderResponse(Order order) {
+        try {
+            PlaceOrderResponse response = new PlaceOrderResponse();
+            response.setTotalPrice(order.getTotalPrice());
+            response.setEstimatedTimeOfDelivery(
+                    DatatypeFactory.newInstance().newXMLGregorianCalendar(
+                            order.getEstimatedTimeOfDelivery().toString()));
+            return response;
+        } catch (DatatypeConfigurationException e) {
+            throw new TypeNotPresentException("XMLGregorianCalendar", e);
+        }
+    }
+
 
     private Map<String, Integer> toHashMap(QuantityMap productQuantities) {
         Map<String, Integer> result = new HashMap<>();
