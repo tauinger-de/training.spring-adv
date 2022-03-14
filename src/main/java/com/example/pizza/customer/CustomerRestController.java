e42f0fea225331745cd427f50d3ff2444a706348
Thomas Auinger <thomas@auinger.de>
2022-03-14 19:24:45 +0100

    added swagger3 support, added @Operation annotations to CustomerRestController

--- src/main/java/com/example/pizza/customer/CustomerRestController.java
+++ src/main/java/com/example/pizza/customer/CustomerRestController.java
@@ -1,5 +1,6 @@
 package com.example.pizza.customer;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
@@ -44,17 +45,20 @@ public class CustomerRestController {
     // --- REST endpoints ---
     //
 
+    @Operation(summary = "Retrieve a single customer", description = "Retrieve a single customer identified by it's numeric id")
     @GetMapping(GET_ONE_ENDPOINT)
     public Customer getCustomer(@PathVariable long id) {
         return this.customerService.getCustomer(id);
     }
 
 
+    @Operation(summary = "Retrieve all customers", description = "Retrieve all customers, no filtering supported yet")
     @GetMapping(GET_ALL_ENDPOINT)
     public Iterable<Customer> getAllCustomers() {
         return this.customerService.getAllCustomers();
     }
 
+    @Operation(summary = "Create customer", description = "Creates a new customer by storing it in persistence. This will auto-generate a numeric customer id")
     @PostMapping(CREATE_ENDPOINT)
     @ResponseStatus(HttpStatus.CREATED)
     public Customer createCustomer(@RequestBody CustomerInDto customerInDto) {
