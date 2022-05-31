51e70f5ac6fbc5cf1e98949e31c0b75639c2e0b4
Thomas Auinger <thomas@auinger.de>
2022-05-31 09:46:09 +0200

    made fields private; added setup() to fix test

--- src/test/java/com/example/pizza/order/OrderServiceTest.java
+++ src/test/java/com/example/pizza/order/OrderServiceTest.java
@@ -1,5 +1,8 @@
 package com.example.pizza.order;
 
+import com.example.pizza.customer.CustomerRepository;
+import com.example.pizza.customer.CustomerSetup;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +13,22 @@ import java.util.Map;
 public class OrderServiceTest {
 
     @Autowired
-    OrderService orderService;
+    private OrderService orderService;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerSetup customerSetup;
+
+    /**
+     * Make sure we have exactly the list of customers as provided by the CustomerSetup.
+     */
+    @BeforeEach
+    public void setup() {
+        customerRepository.deleteAll();
+        customerSetup.createCustomers();
+    }
 
     /**
      * This test executes a simple order placement without any checks/assertions
